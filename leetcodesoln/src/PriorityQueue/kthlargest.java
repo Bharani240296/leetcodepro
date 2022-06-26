9fc56e0f14bb8a11ad71c8bf630a3352692114d9
ramu muralidaran <bharani240296@gmail.com>
2022-06-26 23:23:25 +0530

    inital commit

--- leetcodesoln/src/PriorityQueue/kthlargest.java
+++ leetcodesoln/src/PriorityQueue/kthlargest.java
@@ -2,23 +2,24 @@ package PriorityQueue;
 
 import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.Stack;
 
 public class kthlargest {
-    public static int large(int[] nums, int k) {
-        PriorityQueue<Integer> queue = new PriorityQueue(Collections.reverseOrder());
-        for (int i = 0; i < nums.length; i++) {
-            queue.add(nums[i]);
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] ans = new int[nums1.length];
+        int val = 0;
+
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < nums1.length; i++) {
+            stack.push(nums1[i]);
         }
-        int large = 0;
-        for (int i = 0; i < k; i++) {
-            large = queue.poll();
-        }
-        return large;
+        System.out.println(stack);
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 1, 5, 6, 4};
-        int k = 6;
-        System.out.println(large(nums, k));
+        int[] nums = {4,1,2};
+        int[] nums1={1};
+
     }
 }
